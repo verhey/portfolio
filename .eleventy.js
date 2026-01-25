d05743be138b3407c5511448c89b66255fb701bd
Dean Verhey <deanverhey@gmail.com>
2026-01-24 16:34:14 -0800

    Upgrade 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -42,22 +42,16 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
-  eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
+  // Configure the Eleventy Dev Server (replaces BrowserSync in v3.x)
+  eleventyConfig.setServerOptions({
+    // Disable live reload mirroring of scrolling, clicks, form inputs
+    domDiff: true,
+    port: 8080,
+  });
 
-  /* Build the collection of posts to list in the site
-     - Read the Next Steps post to learn how to extend this
-  */
   eleventyConfig.addCollection("posts", function (collection) {
-    /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
-       - https://www.11ty.dev/docs/collections/
-    */
-
-    // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
-    // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
     const coll = collection.getFilteredByTag("posts");
 
-    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
-    // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
     for (let i = 0; i < coll.length; i++) {
       const prevPost = coll[i - 1];
       const nextPost = coll[i + 1];
